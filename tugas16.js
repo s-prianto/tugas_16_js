b97f1f9322a9b4ff0606a75c4b04f222f067df5d
s-prianto <prianto2796@gmail.com>
2020-02-12 06:37:39 +0700

    Tugas 16 JS

---
+++ tugas16.js
@@ -0,0 +1,8 @@
+function minmax(){
+		var x= Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10 );
+		console.log("Nilai minimal : ", x);
+		var y= Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+		console.log("NIlai maksimal :", y);
+}
+
+minmax()
